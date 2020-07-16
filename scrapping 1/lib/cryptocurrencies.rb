2181da2912b346b4fe94742b843796db57a7855d
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-07-16 23:05:07 +0200

    Add files via upload

--- scrapping 1/lib/cryptocurrencies.rb
+++ scrapping 1/lib/cryptocurrencies.rb
@@ -2,30 +2,31 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
 
-def c_nom
-  c_nom=[]
+def get_cryptos
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+    c_nom=[]
     page.xpath('//*[@class="cmc-table__column-name sc-1kxikfi-0 eTVhdN"]').each do |node|
-    c_nom << node.text
-  end
-return c_nom
+      c_nom << node.text
+    end
+
+
+    c_prix=[]
+    page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"]').each do |node|
+      c_prix << node.text
+    end
+
+    my_hash = Hash.new
+    c_nom.zip(c_prix) { |k,v|  my_hash[k] = v }
+    puts my_hash
+
+    c_result = []
+    my_hash.each do |pair|
+      c_result << pair
+    end
+
+  return c_result
 end
 
-def c_prix
-  c_prix=[]
-  page.url.xpath('//[@class="cmc-tablecell cmc-tablecell--sortable cmc-tablecell--right cmc-tablecell--sort-by__price"]/[@class="cmc-link"]').each do |node|
-  c_prix << node.text
-  end
-return c_prix
-end
-
-my_hash = Hash.new
-c_nom.zip(c_prix) { |k,v|  my_hash[k] = v }
-
-#Ton array devra avoir le format suivant :
-#a = [
-#  { "BTC" => 5245.12 },
-#  { "ETH" => 217.34 },
-#  etc
-#]
+get_cryptos
--- scrapping 1/spec/cryptocurrencies_spec.rb
+++ scrapping 1/spec/cryptocurrencies_spec.rb
@@ -1,12 +1,6 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-
 require_relative '../lib/cryptocurrencies'
-
-describe "nom" do
-
-  describe "prix" do
-    expect "Bitcoin"
-  end
+describe "get_cryptos method " do
+	it "should return an array with hashes" do
+		expect(get_cryptos).not_to be_empty
+	end
 end
